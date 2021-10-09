24575511cc2cab83bf99aba668b5b2cbd443cc90
BenoistP <75934369+BenoistP@users.noreply.github.com>
2021-10-09 10:56:55 +0200

    Updates

---
+++ precache-manifest.c0537cd83a41588b87d6688777528b0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02f50704bb8c0fcca920d97265b6a3c",
+    "url": "/simple-storage-react-truffle-2/index.html"
+  },
+  {
+    "revision": "530f273827d38283668f",
+    "url": "/simple-storage-react-truffle-2/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cb516aec07a8f7156194",
+    "url": "/simple-storage-react-truffle-2/static/js/2.3877faa0.chunk.js"
+  },
+  {
+    "revision": "530f273827d38283668f",
+    "url": "/simple-storage-react-truffle-2/static/js/main.b8b251de.chunk.js"
+  },
+  {
+    "revision": "068fb71a6207ae187589",
+    "url": "/simple-storage-react-truffle-2/static/js/runtime-main.b509cc3f.js"
+  }
+]);
